55341a58dd56ee3bd5640d7c80bd9897375477bd
Jordan LaShomb <jtlashomb9@gmail.com>
2015-09-23 00:49:15 -0400

    server code for host with node - lashomjt,javisoa,yukaj, tombn

---
+++ server.js
@@ -0,0 +1,53 @@
+var http = require("http");
+var fs = require("fs");
+var path = require("path");
+var mime = require("mime");
+var url = require("url");
+
+function send404(response) {
+  response.writeHead(404, {"Content-type" : "text/plain"});
+  response.write("Error 404: resource not found");
+  response.end();
+}
+
+function sendPage(response, filePath, fileContents) {
+  response.writeHead(200, {"Content-type" : mime.lookup(path.basename(filePath))});
+  response.end(fileContents);
+}
+
+function serverWorking(response, absPath) {
+  fs.exists(absPath, function(exists) {
+    if (exists) {
+      fs.readFile(absPath, function(err, data) {
+        if (err) {
+          send404(response)
+        } else {
+          sendPage(response, absPath, data);
+        }
+      });
+    } else {
+      send404(response);
+    }
+  });
+}
+
+var server = http.createServer(function(request, response) {
+  var filePath = false;
+
+  if (request.url == '/') {
+    filePath = "home/lashomjt/SeniorProject/SeniorProject201516/dashplay.html";
+  } else {
+    filePath = "public" + request.url;
+  }
+
+  var absPath = "./" + filePath;
+  serverWorking(response, absPath);
+});
+
+
+http.createServer(function(request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("It's alive!");
+  response.end();
+}).listen(3000);
+
