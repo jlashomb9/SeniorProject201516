c0c6f650480ec57db636f824829f448588d5b240
CSSE Department <csse@mpegdash.csse.rose-hulman.edu>
2015-12-08 21:53:49 -0500

    updated encode to change directory earlier so output is in correct folder

--- node-gpac-dash/encode.sh
+++ node-gpac-dash/encode.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
+cd /home/SeniorProject201516/node-gpac-dash/
+
 DashCast -v $1 -seg-dur 1000 -frag-dur 200 -mpd-refresh 10000 -time-shift -1 -seg-marker eods -min-buffer 0.2 -pixf yuv420p -insert-utc -low-delay -no-loop
 
 IPvar=$(/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
-
-cd /home/SeniorProject201516/node-gpac-dash/
 
 echo $IPvar
 
