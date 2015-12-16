7f4804599a1ce4adae2bc39679f2338c1b5fa180
Brandon Tom <tombn@rose-hulman.edu>
2015-12-15 21:52:20 -0500

    changed encode to start server before encoding

--- node-gpac-dash/encode.sh
+++ node-gpac-dash/encode.sh
@@ -2,10 +2,10 @@
 
 cd /home/SeniorProject201516/node-gpac-dash/
 
-DashCast -v $1 $3
-
 IPvar=$(/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
 
 echo $IPvar
 
 nodejs gpac-dash.js -cors -segment-marker eods -chunk-media-segments -ip $IPvar -port $2
+
+DashCast -v $1 $3
