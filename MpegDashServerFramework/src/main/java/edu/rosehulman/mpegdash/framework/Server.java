839ff90bcc8fbd7a9f58a51967a93eaa6ec156dd
Brandon Tom <tombn@rose-hulman.edu>
2016-01-28 14:01:57 -0500

    ~

--- MpegDashServerFramework/src/main/java/edu/rosehulman/mpegdash/framework/Server.java
+++ MpegDashServerFramework/src/main/java/edu/rosehulman/mpegdash/framework/Server.java
@@ -63,29 +63,28 @@ public class Server implements Runnable{
         BufferedReader input = null;
         String line = null;
         BufferedReader error = null;
-        System.out.println(this.launchCommand);
         String[] cmd = { "/bin/bash", "-c", this.launchCommand };
 
         status = Status.ENCRYPTING;
-//        try {
-//            ls = Runtime.getRuntime().exec(cmd);
-//            input = new BufferedReader(new InputStreamReader(ls.getInputStream()));
-//            error = new BufferedReader(new InputStreamReader(ls.getErrorStream()));
-//        } catch (IOException e1) {
-//            e1.printStackTrace();
-//            System.exit(1);
-//        }
-//        try {
-//            while ((line = input.readLine()) != null)
-//                System.out.println(line);
-//
-//            while ((line = error.readLine()) != null)
-//                System.out.println(line);
-//
-//        } catch (IOException e1) {
-//            e1.printStackTrace();
-//            System.exit(1);
-//        }
+        try {
+            ls = Runtime.getRuntime().exec(cmd);
+            input = new BufferedReader(new InputStreamReader(ls.getInputStream()));
+            error = new BufferedReader(new InputStreamReader(ls.getErrorStream()));
+        } catch (IOException e1) {
+            e1.printStackTrace();
+            System.exit(1);
+        }
+        try {
+            while ((line = input.readLine()) != null)
+                System.out.println(line);
+
+            while ((line = error.readLine()) != null)
+                System.out.println(line);
+
+        } catch (IOException e1) {
+            e1.printStackTrace();
+            System.exit(1);
+        }
 
         status = Status.ENABLED;
     }
