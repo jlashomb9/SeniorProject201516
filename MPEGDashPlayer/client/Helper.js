3b1a5719aad7a0aae0fd2a7215aeeca588463fc7
Jordan LaShomb <jtlashomb9@gmail.com>
2016-02-02 20:35:03 -0500

    frame forward and frame backwards goes closers to a frame now, refactored the the code where we added the button on the video

--- MPEGDashPlayer/client/Helper.js
+++ MPEGDashPlayer/client/Helper.js
@@ -1,15 +1,215 @@
 ButtonHelper = {
-	resetButton : function(){
-		var toggleRunning = document.getElementById('toggleRunning');
-		var shrinkExpand = document.getElementById('expand');
-		console.log(toggleRunning.innerHTML + "" + shrinkExpand.innerHTML);
-		if(toggleRunning.innerHTML === "Play"){
-			toggleRunning.innerHTML = "Pause";
-		}
-		if(shrinkExpand.innerHTML === "Shrink"){
-			shrinkExpand.innerHTML = "Expand";
-		}
+	addPlayPause: function(video){
+		var playButton = document.createElement("BUTTON");        // Create a <button> element
+        var t = document.createTextNode("ll");       // Create a text node
+        playButton.appendChild(t);                
+        playButton.class = "btn btn-default";
+        playButton.id = "play-pause"+ Template.parentData(0)._id;
+        playButton.addEventListener("click", function() {
+          if (video.paused == true) {
+            // Play the video
+            video.play();
 
+            // Update the button text to 'Pause'
+            playButton.innerHTML = "ll";
+          } else {
+            // Pause the video
+            video.pause();
+
+            // Update the button text to 'Play'
+            playButton.innerHTML = "►";
+          }
+        });
+       	return playButton;
+
+
+	},
+	addRewindButton: function(video){
+		var rewindButton = document.createElement("BUTTON");        // Create a <button> element
+        var t = document.createTextNode("<<");       // Create a text node
+        rewindButton.appendChild(t);                
+        rewindButton.class = "btn btn-default";
+        rewindButton.id = "rewind"+ Template.parentData(0)._id;
+		var rwinterval;
+		rewindButton.addEventListener('mousedown',function(e) {
+			rwinterval = setInterval(function() {
+				if(video.currentTime == 0){
+					clearInterval(rwinterval);
+					video.pause();
+				}
+				else{
+					video.currentTime += -1.2;
+				}
+			},360);
+		});
+		rewindButton.addEventListener('mouseup',function(e) {
+			clearInterval(rwinterval);
+		});
+		rewindButton.addEventListener('mouseout',function(e) {
+			clearInterval(rwinterval);
+		});
+		return rewindButton;
+	},
+	addSkipBackButton: function(video){
+		var skipBackButton = document.createElement("BUTTON");        // Create a <button> element
+        var t = document.createTextNode("|<");       // Create a text node
+        skipBackButton.appendChild(t);                
+        skipBackButton.class = "btn btn-default";
+        skipBackButton.id = "skipBack"+ Template.parentData(0)._id;
+        skipBackButton.addEventListener("click", function() {
+          video.currentTime -= .25;
+        });
+        return skipBackButton;
+	},
+	addSkipForwardButton: function(video){
+		var skipForwardButton = document.createElement("BUTTON");        // Create a <button> element
+        var t = document.createTextNode(">|");       // Create a text node
+        skipForwardButton.appendChild(t);                
+        skipForwardButton.class = "btn btn-default";
+        skipForwardButton.id = "skipForward"+ Template.parentData(0)._id;
+        skipForwardButton.addEventListener("click", function() {
+          video.currentTime += .25;
+        });
+
+        return skipForwardButton;
+	},
+	addFastForwardButton: function(video){
+		var fastForwardButton = document.createElement("BUTTON");        // Create a <button> element
+        var t = document.createTextNode(">>");       // Create a text node
+        fastForwardButton.appendChild(t);                
+        fastForwardButton.class = "btn btn-default";
+        fastForwardButton.id = "fastForward"+ Template.parentData(0)._id;
+		var ffinterval;
+		fastForwardButton.addEventListener('mousedown',function(e) {
+			ffinterval = setInterval(function() {
+				video.currentTime += 1.2;
+			},360);
+		});
+		fastForwardButton.addEventListener('mouseup',function(e) {
+			clearInterval(ffinterval);
+		});
+		fastForwardButton.addEventListener('mouseout',function(e) {
+			clearInterval(ffinterval);
+		});
+
+    	return fastForwardButton;
+	},
+	addFullScreenButton: function(video){
+		var fullScreenButton = document.createElement("BUTTON");        // Create a <button> element
+        var t = document.createTextNode("Full Screen");       // Create a text node
+        fullScreenButton.appendChild(t);                
+        fullScreenButton.class = "btn btn-default";
+        fullScreenButton.id = "fullScreen"+ Template.parentData(0)._id;
+        fullScreenButton.addEventListener("click", function() {
+          if (video.requestFullscreen) {
+            video.requestFullscreen();
+          } else if (video.mozRequestFullScreen) {
+            video.mozRequestFullScreen(); // Firefox
+          } else if (video.webkitRequestFullscreen) {
+            video.webkitRequestFullscreen(); // Chrome and Safari
+          }
+        });
+
+        return fullScreenButton;
+	},
+	addSeekBar:function(video){
+		var seekBar = document.createElement("INPUT");
+        seekBar.style.width = "300px";
+        seekBar.id = "seekbar"+Template.parentData(0)._id;
+        seekBar.type = "range";
+        seekBar.value =0;
+        seekBar.style.display="inline-block";
+        // $("#seekbar"+Template.parentData(0)._id).css({"max-width": "360px"});
+        // Event listener for the seek bar
+        seekBar.addEventListener("change", function() {
+          // Calculate the new time
+          var time = video.duration * (seekBar.value / 100);
+
+          // Update the video time
+          video.currentTime = time;
+        });
+
+        
+        // Update the seek bar as the video plays
+        video.addEventListener("timeupdate", function() {
+          // Calculate the slider value
+          var value = (100 / video.duration) * video.currentTime;
+
+          // Update the slider value
+          seekBar.value = value;
+        });
+
+        // Pause the video when the seek handle is being dragged
+        seekBar.addEventListener("mousedown", function() {
+          video.pause();
+        });
+
+        // Play the video when the seek handle is dropped
+        seekBar.addEventListener("mouseup", function() {
+          video.play();
+        });
+
+        return seekBar;
+	},
+	addVolumeBar: function(video){
+		var volumeBar = document.createElement("INPUT");
+        volumeBar.style.width = "60px";
+        volumeBar.id = "volumeBar"+Template.parentData(0)._id;
+        volumeBar.type = "range";
+        volumeBar.min = 0;
+        volumeBar.value = 1;
+        volumeBar.max =1;
+        volumeBar.step = 0.1;
+        volumeBar.style.display = "inline-block";
+        // $("#volumeBar"+Template.parentData(0)._id).css({"max-width": "60px"});
+        // Event listener for the volume bar
+        volumeBar.addEventListener("change", function() {
+          // Update the video volume
+          video.volume = volumeBar.value;
+        });
+        return volumeBar;
+	},
+	addButtonHover: function(id){
+		 $("#playerButtons"+id).css({
+          "position": "absolute",
+          "bottom": 0,
+          "left": 0,
+          "right": 0,
+          "padding": "5px",
+          "opacity": 0,
+          "-webkit-transition": "opacity .3s",
+          "-moz-transition": "opacity .3s",
+          "-o-transition": "opacity .3s",
+          "-ms-transition": "opacity .3s",
+          "transition": "opacity .3s",
+          "background-image": "linear-gradient(bottom, rgb(3,113,168) 13%, rgb(0,136,204) 100%)",
+          "background-image": "-o-linear-gradient(bottom, rgb(3,113,168) 13%, rgb(0,136,204) 100%)",
+          "background-image": "-moz-linear-gradient(bottom, rgb(3,113,168) 13%, rgb(0,136,204) 100%)",
+          "background-image": "-webkit-linear-gradient(bottom, rgb(3,113,168) 13%, rgb(0,136,204) 100%)",
+          "background-image": "-ms-linear-gradient(bottom, rgb(3,113,168) 13%, rgb(0,136,204) 100%)",
+          "background-image": "-webkit-gradient(linear,left bottom, left top,color-stop(0.13, rgb(3,113,168)),color-stop(1, rgb(0,136,204)))"
+        });
+	},
+	addTitleHover: function(id){
+		$("#videoHeader"+id).css({
+          "position": "absolute",
+          "top": 0,
+          "left": 0,
+          "right": 0,
+          "padding": "5px",
+          "opacity": 0,
+          "-webkit-transition": "opacity .3s",
+          "-moz-transition": "opacity .3s",
+          "-o-transition": "opacity .3s",
+          "-ms-transition": "opacity .3s",
+          "transition": "opacity .3s",
+          "background-image": "linear-gradient(bottom, rgb(3,113,168) 13%, rgb(0,136,204) 100%)",
+          "background-image": "-o-linear-gradient(bottom, rgb(3,113,168) 13%, rgb(0,136,204) 100%)",
+          "background-image": "-moz-linear-gradient(bottom, rgb(3,113,168) 13%, rgb(0,136,204) 100%)",
+          "background-image": "-webkit-linear-gradient(bottom, rgb(3,113,168) 13%, rgb(0,136,204) 100%)",
+          "background-image": "-ms-linear-gradient(bottom, rgb(3,113,168) 13%, rgb(0,136,204) 100%)",
+          "background-image": "-webkit-gradient(linear,left bottom, left top,color-stop(0.13, rgb(3,113,168)),color-stop(1, rgb(0,136,204)))"
+        });
 	}
 }
 VideoPlayBackHelper = {
--- MPEGDashPlayer/client/MPEGDashPlayer.js
+++ MPEGDashPlayer/client/MPEGDashPlayer.js
@@ -96,153 +96,21 @@ Template.dashplayer.helpers({
         var video = document.getElementById(video_id);
 
         //play-pause
-        var playButton = document.createElement("BUTTON");        // Create a <button> element
-        var t = document.createTextNode("ll");       // Create a text node
-        playButton.appendChild(t);                
-        playButton.class = "btn btn-default";
-        playButton.id = "play-pause"+ Template.parentData(0)._id;
-        playButton.addEventListener("click", function() {
-          if (video.paused == true) {
-            // Play the video
-            video.play();
-
-            // Update the button text to 'Pause'
-            playButton.innerHTML = "ll";
-          } else {
-            // Pause the video
-            video.pause();
-
-            // Update the button text to 'Play'
-            playButton.innerHTML = "►";
-          }
-        });
+        var playButton = ButtonHelper.addPlayPause(video);
         //rewind
-        var rewindButton = document.createElement("BUTTON");        // Create a <button> element
-        var t = document.createTextNode("<<");       // Create a text node
-        rewindButton.appendChild(t);                
-        rewindButton.class = "btn btn-default";
-        rewindButton.id = "rewind"+ Template.parentData(0)._id;
-		var rwinterval;
-		rewindButton.addEventListener('mousedown',function(e) {
-			rwinterval = setInterval(function() {
-				if(video.currentTime == 0){
-					clearInterval(rwinterval);
-					video.pause();
-				}
-				else{
-					video.currentTime += -1.2;
-				}
-			},360);
-		});
-		rewindButton.addEventListener('mouseup',function(e) {
-			clearInterval(rwinterval);
-		});
-		rewindButton.addEventListener('mouseout',function(e) {
-			clearInterval(rwinterval);
-		});
+        var rewindButton = ButtonHelper.addRewindButton(video);
         //skip backwards
-        var skipBackButton = document.createElement("BUTTON");        // Create a <button> element
-        var t = document.createTextNode("|<");       // Create a text node
-        skipBackButton.appendChild(t);                
-        skipBackButton.class = "btn btn-default";
-        skipBackButton.id = "skipBack"+ Template.parentData(0)._id;
-        skipBackButton.addEventListener("click", function() {
-          video.currentTime -= 1;
-        });
+        var skipBackButton = ButtonHelper.addSkipBackButton(video);
         //skip forward
-        var skipForwardButton = document.createElement("BUTTON");        // Create a <button> element
-        var t = document.createTextNode(">|");       // Create a text node
-        skipForwardButton.appendChild(t);                
-        skipForwardButton.class = "btn btn-default";
-        skipForwardButton.id = "skipForward"+ Template.parentData(0)._id;
-        skipForwardButton.addEventListener("click", function() {
-          video.currentTime += 1;
-        });
+        var skipForwardButton = ButtonHelper.addSkipForwardButton(video);
         //fast forward
-        var fastForwardButton = document.createElement("BUTTON");        // Create a <button> element
-        var t = document.createTextNode(">>");       // Create a text node
-        fastForwardButton.appendChild(t);                
-        fastForwardButton.class = "btn btn-default";
-        fastForwardButton.id = "fastForward"+ Template.parentData(0)._id;
-		var ffinterval;
-		fastForwardButton.addEventListener('mousedown',function(e) {
-			ffinterval = setInterval(function() {
-				video.currentTime += 1.2;
-			},360);
-		});
-		fastForwardButton.addEventListener('mouseup',function(e) {
-			clearInterval(ffinterval);
-		});
-		fastForwardButton.addEventListener('mouseout',function(e) {
-			clearInterval(ffinterval);
-		});
+        var fastForwardButton = ButtonHelper.addFastForwardButton(video);
         //full screen
-        var fullScreenButton = document.createElement("BUTTON");        // Create a <button> element
-        var t = document.createTextNode("Full Screen");       // Create a text node
-        fullScreenButton.appendChild(t);                
-        fullScreenButton.class = "btn btn-default";
-        fullScreenButton.id = "fullScreen"+ Template.parentData(0)._id;
-        fullScreenButton.addEventListener("click", function() {
-          if (video.requestFullscreen) {
-            video.requestFullscreen();
-          } else if (video.mozRequestFullScreen) {
-            video.mozRequestFullScreen(); // Firefox
-          } else if (video.webkitRequestFullscreen) {
-            video.webkitRequestFullscreen(); // Chrome and Safari
-          }
-        });
+        var fullScreenButton = ButtonHelper.addFullScreenButton(video);
         //seekbar
-        var seekBar = document.createElement("INPUT");
-        seekBar.style.width = "300px";
-        seekBar.id = "seekbar"+Template.parentData(0)._id;
-        seekBar.type = "range";
-        seekBar.value =0;
-        seekBar.style.display="inline-block";
-        // $("#seekbar"+Template.parentData(0)._id).css({"max-width": "360px"});
-        // Event listener for the seek bar
-        seekBar.addEventListener("change", function() {
-          // Calculate the new time
-          var time = video.duration * (seekBar.value / 100);
-
-          // Update the video time
-          video.currentTime = time;
-        });
-
-        
-        // Update the seek bar as the video plays
-        video.addEventListener("timeupdate", function() {
-          // Calculate the slider value
-          var value = (100 / video.duration) * video.currentTime;
-
-          // Update the slider value
-          seekBar.value = value;
-        });
-
-        // Pause the video when the seek handle is being dragged
-        seekBar.addEventListener("mousedown", function() {
-          video.pause();
-        });
-
-        // Play the video when the seek handle is dropped
-        seekBar.addEventListener("mouseup", function() {
-          video.play();
-        });
+        var seekBar = ButtonHelper.addSeekBar(video);
         //volume bar
-        var volumeBar = document.createElement("INPUT");
-        volumeBar.style.width = "60px";
-        volumeBar.id = "volumeBar"+Template.parentData(0)._id;
-        volumeBar.type = "range";
-        volumeBar.min = 0;
-        volumeBar.value = 1;
-        volumeBar.max =1;
-        volumeBar.step = 0.1;
-        volumeBar.style.display = "inline-block";
-        // $("#volumeBar"+Template.parentData(0)._id).css({"max-width": "60px"});
-        // Event listener for the volume bar
-        volumeBar.addEventListener("change", function() {
-          // Update the video volume
-          video.volume = volumeBar.value;
-        });
+        var volumeBar = ButtonHelper.addVolumeBar(video);
 
         var buttonList = document.getElementById("playerButtons"+Template.parentData(0)._id);
         //Appending all buttons
@@ -254,47 +122,12 @@ Template.dashplayer.helpers({
         buttonList.appendChild(seekBar);
         buttonList.appendChild(fullScreenButton);
         buttonList.appendChild(volumeBar);
+        var id = Template.parentData(0)._id;
         
-
-        $("#playerButtons"+Template.parentData(0)._id).css({
-          "position": "absolute",
-          "bottom": 0,
-          "left": 0,
-          "right": 0,
-          "padding": "5px",
-          "opacity": 0,
-          "-webkit-transition": "opacity .3s",
-          "-moz-transition": "opacity .3s",
-          "-o-transition": "opacity .3s",
-          "-ms-transition": "opacity .3s",
-          "transition": "opacity .3s",
-          "background-image": "linear-gradient(bottom, rgb(3,113,168) 13%, rgb(0,136,204) 100%)",
-          "background-image": "-o-linear-gradient(bottom, rgb(3,113,168) 13%, rgb(0,136,204) 100%)",
-          "background-image": "-moz-linear-gradient(bottom, rgb(3,113,168) 13%, rgb(0,136,204) 100%)",
-          "background-image": "-webkit-linear-gradient(bottom, rgb(3,113,168) 13%, rgb(0,136,204) 100%)",
-          "background-image": "-ms-linear-gradient(bottom, rgb(3,113,168) 13%, rgb(0,136,204) 100%)",
-          "background-image": "-webkit-gradient(linear,left bottom, left top,color-stop(0.13, rgb(3,113,168)),color-stop(1, rgb(0,136,204)))"
-        });
+       ButtonHelper.addButtonHover(id);
+       ButtonHelper.addTitleHover(id);
 		
-		$("#videoHeader"+Template.parentData(0)._id).css({
-          "position": "absolute",
-          "top": 0,
-          "left": 0,
-          "right": 0,
-          "padding": "5px",
-          "opacity": 0,
-          "-webkit-transition": "opacity .3s",
-          "-moz-transition": "opacity .3s",
-          "-o-transition": "opacity .3s",
-          "-ms-transition": "opacity .3s",
-          "transition": "opacity .3s",
-          "background-image": "linear-gradient(bottom, rgb(3,113,168) 13%, rgb(0,136,204) 100%)",
-          "background-image": "-o-linear-gradient(bottom, rgb(3,113,168) 13%, rgb(0,136,204) 100%)",
-          "background-image": "-moz-linear-gradient(bottom, rgb(3,113,168) 13%, rgb(0,136,204) 100%)",
-          "background-image": "-webkit-linear-gradient(bottom, rgb(3,113,168) 13%, rgb(0,136,204) 100%)",
-          "background-image": "-ms-linear-gradient(bottom, rgb(3,113,168) 13%, rgb(0,136,204) 100%)",
-          "background-image": "-webkit-gradient(linear,left bottom, left top,color-stop(0.13, rgb(3,113,168)),color-stop(1, rgb(0,136,204)))"
-        });
+		    
         // $("#"+video_id+":hover "+"#playerButtons"+Template.parentData(0)._id).css({"opacity": .9});
         var jqueryVideo = $("#draggable"+Template.parentData(0)._id);
         var jqueryPlayerButtons = $("#playerButtons"+Template.parentData(0)._id);
