170fe9519f8527b5faadbd0785e1bbff433d5ad6
Brandon Tom <tombn@rose-hulman.edu>
2016-02-16 20:26:27 -0500

    ~

--- MpegDashServerFramework/src/main/java/edu/rosehulman/mpegdash/framework/CommandLineInterface.java
+++ MpegDashServerFramework/src/main/java/edu/rosehulman/mpegdash/framework/CommandLineInterface.java
@@ -2,20 +2,22 @@ package edu.rosehulman.mpegdash.framework;
 
 import java.util.Scanner;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
 
 import com.beust.jcommander.JCommander;
 
-import edu.rosehulman.mpegdash.framework.ServerLauncher;
-
-@SpringBootApplication
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
 public class CommandLineInterface {
 
     private static final Logger LOGGER = LogManager.getLogger(CommandLineInterface.class);
