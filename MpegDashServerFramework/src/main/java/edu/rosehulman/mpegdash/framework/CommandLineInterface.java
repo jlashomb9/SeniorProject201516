37dbaa97a68cb28a3563c0248b74ef242c454079
Brandon Tom <tombn@rose-hulman.edu>
2016-02-16 20:44:17 -0500

    ~

--- MpegDashServerFramework/src/main/java/edu/rosehulman/mpegdash/framework/CommandLineInterface.java
+++ MpegDashServerFramework/src/main/java/edu/rosehulman/mpegdash/framework/CommandLineInterface.java
@@ -5,19 +5,15 @@ import java.util.Scanner;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import com.beust.jcommander.JCommander;
 
-@Configuration
-@ComponentScan
-@EnableAutoConfiguration
+@SpringBootApplication
 public class CommandLineInterface {
 
     private static final Logger LOGGER = LogManager.getLogger(CommandLineInterface.class);
--- MpegDashServerFramework/src/main/java/edu/rosehulman/mpegdash/framework/GreetingController.java
+++ MpegDashServerFramework/src/main/java/edu/rosehulman/mpegdash/framework/GreetingController.java
@@ -1,18 +1,21 @@
 package edu.rosehulman.mpegdash.framework;
 
 import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
+@RequestMapping("/greeting")
 public class GreetingController {
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("/greeting")
+    @RequestMapping(method=RequestMethod.GET)
     public @ResponseBody Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new Greeting(counter.incrementAndGet(),
                             String.format(template, name));
