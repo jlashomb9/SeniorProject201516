e52445023d6243ea639d256684b3c036e59b70f4
jarvisoa <jarvisoa@rose-hulman.edu>
2016-03-30 12:50:53 -0400

    removed some unused files and code, fixed css (NO MORE WHITE BARS AROUND VIDEO), added a few comments

--- MPEGDashPlayer/client/Helper.js
+++ MPEGDashPlayer/client/Helper.js
@@ -1,17 +1,3 @@
-ButtonHelper = {
-	resetButton : function(){
-		var toggleRunning = document.getElementById('toggleRunning');
-		var shrinkExpand = document.getElementById('expand');
-		console.log(toggleRunning.innerHTML + "" + shrinkExpand.innerHTML);
-		if(toggleRunning.innerHTML === "Play"){
-			toggleRunning.innerHTML = "Pause";
-		}
-		if(shrinkExpand.innerHTML === "Shrink"){
-			shrinkExpand.innerHTML = "Expand";
-		}
-
-	}
-}
 VideoPlayBackHelper = {
 	createVideo: function(video,hostName){
 		var context = new Dash.di.DashContext();
